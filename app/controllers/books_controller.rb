b605a0dd23645cbe429c40739eeb3c6d985f92d8
vagrant <vagrant@localhost.localdomain>
2020-12-09 15:16:59 +0000

    [update]コントローラー名修正blogsâからbooksへ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,15 +1,15 @@
-class BlogsController < ApplicationController
-  
+class BooksController < ApplicationController
+
+  def top
+  end
+
   def index
   end
 
-  def show
-  end
-
-  def new
-  end
-
   def create
+  end
+
+  def show
   end
 
   def edit
